75707ae988cf924d9eab3ab9996ab62c98e706a8
mr-SCWN <2004ivanko@gmail.com>
2025-03-13 14:39:29 +0100

    deleted unnecessary files

--- GameFestival/Assets/Scripts/PlayerController.cs
+++ GameFestival/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Скорость движения
+    public float moveSpeed = 5f; // Movement Speed 
 
     void Update()
     {
