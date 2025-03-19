31d48af5c148b95650007032a0a770b942b6ce71
mr-SCWN <2004ivanko@gmail.com>
2025-03-19 14:41:06 +0100

    Added infoboard and triggers

---
+++ GameFestival/Assets/Scripts/Trigger codes/BoardTrigger.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class BoardTrigger : MonoBehaviour
+{
+    [Header ("UI Elements")]
+    [Tooltip("Main window with text, that shows after clicking E")]
+    public GameObject infoPanel;
+    
+    [Tooltip("Small tip, click E")]
+    public GameObject pressEHint;
+
+    private bool playerInRange = false;
+    private bool panelOpen = false;
+
+    private void Start(){
+        if (infoPanel!=null){
+            infoPanel.SetActive(false);
+        }
+        if (pressEHint != null){
+            pressEHint.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D other){
+        if (other.CompareTag("Player")){
+            playerInRange = true;
+            if (pressEHint != null){
+                pressEHint.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D other){
+        if (other.CompareTag("Player")){
+            playerInRange = false;
+            if (pressEHint!=null){
+                pressEHint.SetActive(false);
+            }
+            if (panelOpen && infoPanel != null){
+                panelOpen = false;
+                infoPanel.SetActive(false);
+            }
+        }
+    }
+
+
+    private void Update (){
+        if (playerInRange && Input.GetKeyDown(KeyCode.E)){
+            panelOpen = !panelOpen;
+
+            if (infoPanel!=null){
+                infoPanel.SetActive(panelOpen);
+            }
+        }
+    }
+}
