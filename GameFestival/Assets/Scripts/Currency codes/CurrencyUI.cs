6ed167ccbd874e9b2ae4ef6b900c9090ae4afaa6
mr-SCWN <2004ivanko@gmail.com>
2025-03-24 14:25:37 +0100

    added codes for Currency

---
+++ GameFestival/Assets/Scripts/Currency codes/CurrencyUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CurrencyUI : MonoBehaviour
+{
+    [Header("Text fields for each currency")]
+    public TextMeshProUGUI coin;
+    public TextMeshProUGUI grey_fish;
+    public TextMeshProUGUI green_fish;
+    public TextMeshProUGUI gold_fish;
+
+    void toUpdate () {
+        coin.text = CurrencyManager.Instance.coin.ToString();
+        grey_fish.text = CurrencyManager.Instance.grey_fish.ToString();
+        green_fish.text = CurrencyManager.Instance.green_fish.ToString();
+        gold_fish.text = CurrencyManager.Instance.gold_fish.ToString();
+        
+    }
+} 
