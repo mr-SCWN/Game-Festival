ae030052e8de2b185858206c3c0e91cbd742f748
Yana;) <yana2003333@gmail.com>
2025-03-29 19:06:02 +0100

    Added player with animation

--- GameFestival/Assets/Scripts/Player codes/PlayerController.cs
+++ GameFestival/Assets/Scripts/Player codes/PlayerController.cs
@@ -5,13 +5,15 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f; 
     private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 movement;
+    private float lastMoveX = 1f;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-       
-        
+        animator = GetComponent<Animator>();
+
         rb.freezeRotation = true; 
     }
 
@@ -20,6 +22,18 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         movement = new Vector2(moveX, moveY).normalized;
+
+        if (moveX != 0)
+        {
+            lastMoveX = moveX;
+        }
+
+        float animationX = (moveX == 0 && moveY != 0) ? lastMoveX : moveX;
+
+        animator.SetFloat("MoveX", animationX);
+        animator.SetFloat("MoveY", moveY);
+        animator.SetBool("IsMoving", movement.magnitude > 0.01f);
+
     }
 
     void FixedUpdate()
